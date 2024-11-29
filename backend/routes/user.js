5f1b61c14ab4f696f763442d9a5977f60dfd9f56
kylmariecabungcal <kylamariecabungcal4@gmail.com>
2024-11-29 13:14:37 +0800

    login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,25 +1,9 @@
 const express = require("express");
-const {
-    registerUser,
-    getAllUsers,
-    deleteUser,
-    updateUser,
-    getUser,
-} = require("../controllers/userController");
+const { loginUser } = require("../controllers/userController");
 
 const router = express.Router();
 
-// Register a new user
-router.post("/register", registerUser);
 
-// Get all users
-router.get("/", getAllUsers);
-router.get('/users/:id', getUser);
-
-// Delete a user by ID
-router.delete("/:id", deleteUser);
-
-// Update a user by ID
-router.put("/:id", updateUser);
+router.post('/login', loginUser);  
 
 module.exports = router;
