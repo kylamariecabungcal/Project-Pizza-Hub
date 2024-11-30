98e3fe94230cb4c92e3967de6c799d3efbc73baa
neil761 <alvarezneilandrew486@gmail.com>
2024-11-30 12:15:17 +0800

    mata

--- frontend/script/user.js
+++ frontend/script/user.js
@@ -33,3 +33,19 @@ document.getElementById('loginForm').addEventListener('submit', async (event) =>
     messageDiv.style.color = 'red';
   }
 });
+
+function togglePassword() {
+  const passwordField = document.getElementById("password");
+  const toggleIcon = document.getElementById("toggle-icon");
+  
+  if (passwordField.type === "password") {
+      passwordField.type = "text";
+      toggleIcon.classList.remove("bi-eye");
+      toggleIcon.classList.add("bi-eye-slash");
+  } else {
+      passwordField.type = "password";
+      toggleIcon.classList.remove("bi-eye-slash");
+      toggleIcon.classList.add("bi-eye");
+  }
+}
+
